1269d0c02a4ba04e6e885318e7fa227f4e059bb7
Ryan Chen <ryanchen979@gmail.com>
2023-02-15 10:16:04 -0500

    a

--- create-task-project/javascript/main.js
+++ create-task-project/javascript/main.js
@@ -1,1 +1,9 @@
 import "../style/style.css";
+const url = "www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+
+async function food(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}
+food();
