64af60f9d2cf490f92dfe62aac50f8929d122c1e
Ryan Chen <ryanchen979@gmail.com>
2023-02-16 10:16:39 -0500

    z

--- create-task-project/javascript/main.js
+++ create-task-project/javascript/main.js
@@ -1,9 +1,17 @@
 import "../style/style.css";
-const url = "www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+const url = "https://www.boredapi.com/api/activity/";
 
 async function food(url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  console.log(data);
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(`Activity: ${data.activity}`);
+    console.log(`Type: ${data.type}`);
+    console.log(`Participants: ${data.participants}`);
+    console.log(`Accessibility: ${data.accessibility}`);
+  } catch (error) {
+    console.error("There's a problem with the API");
+  }
 }
-food();
+food(url);
