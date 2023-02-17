d723154896720317d92befee772c5045bf62554c
Ryan Chen <ryanchen979@gmail.com>
2023-02-17 10:00:54 -0500

    a

---
+++ create-task-project/javascript/dom.js
@@ -0,0 +1,3 @@
+export const dom = {
+  content: document.querySelector(".content"),
+};
--- create-task-project/javascript/main.js
+++ create-task-project/javascript/main.js
@@ -1,4 +1,5 @@
 import "../style/style.css";
+import { dom } from "./dom.js";
 const url = "https://www.boredapi.com/api/activity/";
 
 async function food(url) {
@@ -10,6 +11,14 @@ async function food(url) {
     console.log(`Type: ${data.type}`);
     console.log(`Participants: ${data.participants}`);
     console.log(`Accessibility: ${data.accessibility}`);
+
+    dom.content.insertAdjacentHTML(
+      "beforeend",
+      `<div class="activity">Activity: ${data.activity}</div>
+      <div class="type">Type: ${data.type}</div>
+      <div class="participants">Participants: ${data.participants}</div>
+      <div class="accessibility">Accessibility: ${data.accessibility}</div>`
+    );
   } catch (error) {
     console.error("There's a problem with the API");
   }
