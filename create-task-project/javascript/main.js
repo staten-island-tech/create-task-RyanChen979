7763009b17a09227b9efb85e70145719b79dcbf5
Ryan Chen <ryanchen979@gmail.com>
2023-02-21 13:36:42 -0500

    o

--- create-task-project/javascript/main.js
+++ create-task-project/javascript/main.js
@@ -28,13 +28,14 @@ async function suggest(url) {
 suggest(url);
 
 dom.buttonNew.addEventListener(onclick, function () {
-  dom.content.insertAdjacentHTML(
-    "beforeend",
-    `
+  suggest(url);
+});
+/* dom.content.insertAdjacentHTML(
+  "beforeend",
+  `
     <div class="activity">Activity: ${data.activity}</div>
     <div class="type">Type: ${data.type}</div>
     <div class="participants">Participants: ${data.participants}</div>
     <div class="accessibility">Accessibility: ${data.accessibility}</div>
     `
-  );
-});
+); */
