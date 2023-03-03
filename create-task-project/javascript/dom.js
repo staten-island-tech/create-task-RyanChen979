fd1c1af9a7df79800c38e7cace644921f1573f5d
Ryan Chen <ryanchen979@gmail.com>
2023-03-03 00:31:09 -0500

    ah

--- create-task-project/javascript/dom.js
+++ create-task-project/javascript/dom.js
@@ -1,4 +1,6 @@
 export const DOM = {
   content: document.querySelector(".content"),
   buttonNew: document.querySelector(".new-suggestion"),
+  buttonTen: document.querySelector(".ten-suggestions"),
+  historyButton: document.querySelector(".history-tab"),
 };
--- create-task-project/javascript/main.js
+++ create-task-project/javascript/main.js
@@ -1,7 +1,7 @@
 import "../style/style.css";
 import { DOM } from "./dom.js";
 const url = "https://www.boredapi.com/api/activity/";
-const activitySuggestions = [];
+let activityHistory = [];
 
 async function suggest(url) {
   try {
@@ -29,16 +29,45 @@ async function suggest(url) {
       </div>
       `
     );
+
+    activityHistory.push(activityData);
+    console.log(activityHistory);
   } catch (error) {
     console.error(error);
   }
 }
 suggest(url);
 
-function push2History() {}
-
 DOM.buttonNew.addEventListener("click", function (e) {
   e.preventDefault();
   DOM.content.innerHTML = "";
   suggest(url);
+});
+
+DOM.buttonTen.addEventListener("click", function (e) {
+  e.preventDefault();
+  DOM.content.innerHTML = "";
+  for (let x = 0; x < 10; x++) suggest(url);
+});
+
+DOM.historyButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  DOM.content.innerHTML = "";
+  activityHistory.forEach((el) =>
+    DOM.content.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="box">
+        <div class="activity"><h2>Activity: ${el.activity}</h2></div>
+        <div class="type"><h3>Type: ${el.type}</h3></div>
+        <div class="participants">
+          <h3>Participants: ${el.participants}</h3>
+        </div>
+        <div class="accessibility">
+          <h3>Accessibility: ${el.accessibility}</h3>
+        </div>
+      </div>
+      `
+    )
+  );
 });
